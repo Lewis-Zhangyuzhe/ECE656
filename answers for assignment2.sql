bef31043837cebce883483a7ffaf772894a0c59c
y2877zha <y2877zha@uwaterloo.ca>
2019-03-06 23:51:53 -0500

    a and b answers for the assignment2

---
+++ answers for assignment2.sql
@@ -0,0 +1,14 @@
+## a.
+#  instID is the primary key of table Instructor
+#  courseID is the primary key of table Course
+#  the combination of courseID and section is the primary key of table Offering
+#  roomID is the primary key of table Classroom
+#  deptID is the primary ket of table Department
+
+## b.
+#  instID in the table Offering is the foreign key of table Instructor
+#  deptID in the table Instructor is the foreign key of table Department
+#  courseID in the table Offering is the foreign key of table Course
+#  deptID in the table Course is the foreign key of table Department
+#  roomID in the table Offering is the foreign key of table Classroom
+# 
